4b03414f48be091f6dc04d428ba0acccb08b11e7
Brenleigh Killpack <kil21008@byui.edu>
2024-03-12 18:04:50 -0600

    number guessing game

--- number-guessing.py
+++ number-guessing.py
@@ -1,1 +1,15 @@
-print("Hello, world.")
+import random
+
+random_number = random.randint(1, 100)
+user_guess = 0
+
+while user_guess != random_number:
+    user_guess = int(input("Guess a number between 1 and 100: "))
+
+    if (user_guess > random_number):
+        print("Lower")
+    if (user_guess < random_number):
+        print("Higher")
+    if (user_guess == random_number):
+        print("Good job! You guessed it!")
+        break
